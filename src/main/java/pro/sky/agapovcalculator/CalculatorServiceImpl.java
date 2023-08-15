e6f75dafa305be6d9a192f3b5d64d5f1ef8769ae
iShantaram <1agapovav987@gmail.com>
2023-08-15 13:41:54 +0300

    Made controller class, with 5 GetMappings-methods. Made interface with 4 mathematical methods. Released them in CalculatorServiceImpl class.
    Мне кажется, стоит запихнуть проверку деления на 0 в метод divide в Service, но тогда там надо вызывать throw, чтобы прервать выполнение программы вручную, и метод divide не дошел до return. А в текущей реализации ее ловит контроллер.
    На уроке метод hello реализовывался в сервисе. Но в дз написано, что контроллер формирует ответную строку, поэтому (по логике из дз) строку приветствия я запихнул также в контроллер.

---
+++ src/main/java/pro/sky/agapovcalculator/CalculatorServiceImpl.java
@@ -0,0 +1,27 @@
+package pro.sky.agapovcalculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+    @Override
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    @Override
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
