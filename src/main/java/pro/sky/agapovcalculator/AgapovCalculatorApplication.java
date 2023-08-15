d8c900b587caf992ce9123204aefff35fb525bb8
iShantaram <1agapovav987@gmail.com>
2023-08-15 13:22:49 +0300

    Initial commit

---
+++ src/main/java/pro/sky/agapovcalculator/AgapovCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.agapovcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgapovCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgapovCalculatorApplication.class, args);
+	}
+
+}
