b2934bf13c059bb1e3e7b7806ed8af9b29195734
iShantaram <1agapovav987@gmail.com>
2023-08-20 20:17:56 +0300

    добавлено: @RequestMapping(path = "/calculator") - в класс-контроллер.

--- src/main/java/pro/sky/agapovcalculator/CalculatorController.java
+++ src/main/java/pro/sky/agapovcalculator/CalculatorController.java
@@ -1,9 +1,10 @@
 package pro.sky.agapovcalculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping(path = "/calculator")
 @RestController
 public class CalculatorController {
     private final String PRIVETSTVIE = "Добро пожаловать в калькулятор!";
